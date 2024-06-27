22540c429a66aba2fee1aa7a1e9eba0b62369179
iKosMooh <72697898+iKosMooh@users.noreply.github.com>
2024-06-27 16:11:22 -0300

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
