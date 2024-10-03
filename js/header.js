43c3b9097bf1869f70f9cecbbea0033f2cca72a4
iKosMooh <caiogamerof@gmail.com>
2024-10-03 20:18:05 -0300

    03/10 Guh Vacilão Faltando...

--- js/header.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-navToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
