be4cdace630a38fffb5db7e5f92d855b8a14d58c
henriquesilva22 <henrique989151620@gmail.com>
2024-10-14 00:06:14 -0300

    Update formulariocoordenador.php

--- pages/formulariocoordenador.php
+++ pages/formulariocoordenador.php
@@ -2,13 +2,14 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seleção de Professor e Aulas</title>
-    <link rel="stylesheet" href="../css/formcoordarnador.css"> <!-- Adicione o link para o CSS -->
+    <link rel="stylesheet" href="../css/formcoordarnador.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+    <?php include('../pages/navcoord.php');?>
 </head>
 <body>
-<div class="container"> <!-- Container para centralizar o conteúdo -->
+<div class="container">
     <label for="professores">Selecione um Professor:</label>
     <select name="professores" id="professores">
         <option value="disabled" disabled selected>Selecione um Professor</option>
@@ -33,7 +34,6 @@
 
 <script>
 $(document).ready(function() {
-    // Adicionar conteúdo padrão para visualização de CSS
     function preencherConteudoSimulado() {
         $('#aulas').append('<option value="1">2024-09-20 14:00 - Matemática - Curso A</option>');
         var formulario = `
@@ -50,7 +50,6 @@ $(document).ready(function() {
         $('#formulario_reposicao').html(formulario);
     }
 
-    // Preencher conteúdo simulado ao carregar a página
     preencherConteudoSimulado();
 
     $('#professores').change(function() {
@@ -138,6 +137,32 @@ $(document).ready(function() {
 });
 </script>
 
-
+<footer>
+    <div id="footer_content">
+        <div id="footer_contacts">
+            <p>
+                Faculdade de Tecnologia de São Paulo - Educação profissional pública<br>
+                superior e de excelência!
+            </p>
+        </div>
+        <div id="footer_social_media">
+            <ul>
+                <li><a href="https://wa.me/551938635210" class="footer_link" id="whatsapp" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
+                <li><a href="https://www.facebook.com/fatecitapira?locale=pt_BR" class="footer_link" id="facebook" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/fatecdeitapira/" class="footer_link" id="instagram" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="https://www.linkedin.com/company/fatec-de-itapira/?originalSubdomain=br" class="footer_link" id="linkedin" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="footer_logos">
+        <a href="https://www.cps.sp.gov.br/"> 
+            <img src="../img/cps-removebg-preview.png" alt="Logo do Centro Paula Souza" class="footer-logo">
+        </a>
+        <a href="https://www.saopaulo.sp.gov.br/"> 
+            <img src="../img/logo-footer-governo-do-estado-sp.png" alt="Logo do Governo do Estado de São Paulo" class="footer-logo middle">
+        </a>
+    </div>
+    <p class="footer-rights">© Todos os direitos reservados</p>
+</footer>
 </body>
 </html>
