60a6d0f8894b52368e68b41f1bbff3f2c655c3fc
Gustavo Ribeiro <gustavosley69@gmail.com>
2024-10-18 21:27:01 -0300

    Update formulariocoordenador.php

--- pages/formulariocoordenador.php
+++ pages/formulariocoordenador.php
@@ -6,7 +6,7 @@
     <title>Seleção de Professor e Aulas</title>
     <link rel="stylesheet" href="../css/formcoordarnador.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <?php include('../pages/navcoord.php');?>
+    <?php include('header.php');?>
 </head>
 <body>
 <div class="container">
@@ -137,32 +137,6 @@ $(document).ready(function() {
 });
 </script>
 
-<footer>
-    <div id="footer_content">
-        <div id="footer_contacts">
-            <p>
-                Faculdade de Tecnologia de São Paulo - Educação profissional pública<br>
-                superior e de excelência!
-            </p>
-        </div>
-        <div id="footer_social_media">
-            <ul>
-                <li><a href="https://wa.me/551938635210" class="footer_link" id="whatsapp" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
-                <li><a href="https://www.facebook.com/fatecitapira?locale=pt_BR" class="footer_link" id="facebook" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-                <li><a href="https://www.instagram.com/fatecdeitapira/" class="footer_link" id="instagram" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
-                <li><a href="https://www.linkedin.com/company/fatec-de-itapira/?originalSubdomain=br" class="footer_link" id="linkedin" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
-            </ul>
-        </div>
-    </div>
-    <div id="footer_logos">
-        <a href="https://www.cps.sp.gov.br/"> 
-            <img src="../img/cps-removebg-preview.png" alt="Logo do Centro Paula Souza" class="footer-logo">
-        </a>
-        <a href="https://www.saopaulo.sp.gov.br/"> 
-            <img src="../img/logo-footer-governo-do-estado-sp.png" alt="Logo do Governo do Estado de São Paulo" class="footer-logo middle">
-        </a>
-    </div>
-    <p class="footer-rights">© Todos os direitos reservados</p>
-</footer>
+<?php include_once 'footer.php'; ?>
 </body>
 </html>
