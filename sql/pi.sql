5ab4341aa85b8750f65c1166556aa403310d518d
iKosMooh <caiogamerof@gmail.com>
2024-11-14 19:20:57 -0300

    Banco faltando popular com correções da ana

--- sql/pi.sql
+++ sql/pi.sql
@@ -3,13 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/11/2024 às 00:46
+-- Tempo de geração: 14/11/2024 às 23:20
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "-03:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
